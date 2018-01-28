19caa54a30a3021d1a7af0aa4502be4162a3ac36
Paren Desai <paren1994@yahoo.com.au>
2018-01-28 09:30:29 -0500

    Presentable endpoints

--- entry.js
+++ entry.js
@@ -5,97 +5,93 @@ console.log("hsdfsdf")
 var photo;
 var secret;
 const model = new KerasJS.Model({
-  filepath: 'final_hide_212.bin',
-  gpu: true
+    filepath: 'final_hide_212.bin',
+    gpu: true
 })
 var d = new Date();
 var output;
 var image
 
 var predict_function = function() {
-  return model.predict({
+    return model.predict({
         'hide_in': new Float32Array(photo),
-        'prep_in': new Float32Array(secret), 
+        'prep_in': new Float32Array(secret),
 
-})
+    })
 }
 model.ready()
-  .then(() => {
-    console.log("ready")
-    $('#loading').hide();
-    $('#loaded').show();
-    $('#predict').on('click', function(){
-  var c = document.createElement("canvas");
-        c.width = 224
-        c.height = 224;
-        var ctx = c.getContext("2d");
-        var img = $('.file-upload-image1')[0];
-        ctx.drawImage(img, 0, 0);
-        var imgData = ctx.getImageData(0, 0, c.width, c.height);
-        photo = new Float32Array(224 * 224 * 3)
-        var k = 0;
-        for (var i = 0; i < imgData.data.length; i += 4) {
-            photo[k++] = (((imgData.data[i]) / 255.0) - 0.485) / 0.229;
-            photo[k++] = (((imgData.data[i + 1]) / 255.0) - 0.456) / 0.224;
-            photo[k++] = (((imgData.data[i + 2]) / 255.0) - 0.406) / 0.225;
-        }
+    .then(() => {
+        console.log("ready")
+        $('#loading').hide();
+        $('#loaded').show();
+        $('#predict').on('click', function() {
+            var c = document.createElement("canvas");
+            c.width = 224
+            c.height = 224;
+            var ctx = c.getContext("2d");
+            var img = $('.file-upload-image1')[0];
+            ctx.drawImage(img, 0, 0);
+            var imgData = ctx.getImageData(0, 0, c.width, c.height);
+            photo = new Float32Array(224 * 224 * 3)
+            var k = 0;
+            for (var i = 0; i < imgData.data.length; i += 4) {
+                photo[k++] = (((imgData.data[i]) / 255.0) - 0.485) / 0.229;
+                photo[k++] = (((imgData.data[i + 1]) / 255.0) - 0.456) / 0.224;
+                photo[k++] = (((imgData.data[i + 2]) / 255.0) - 0.406) / 0.225;
+            }
 
-        var c = document.createElement("canvas");
-        c.width = 224
-        c.height = 224;
-        var ctx = c.getContext("2d");
-        var img = $('.file-upload-image2')[0];
-        ctx.drawImage(img, 0, 0);
-        var imgData = ctx.getImageData(0, 0, c.width, c.height);
-        secret = new Float32Array(224 * 224 * 3)
-        var k = 0;
-        for (var i = 0; i < imgData.data.length; i += 4) {
-            secret[k++] = (((imgData.data[i]) / 255.0) - 0.485) / 0.229;
-            secret[k++] = (((imgData.data[i + 1]) / 255.0) - 0.456) / 0.224;
-            secret[k++] = (((imgData.data[i + 2]) / 255.0) - 0.406) / 0.225;
-        }
-          predict_function().then(outputData => {
-          output = outputData['hide_out']
-          console.log(output)
-          image = new Uint8ClampedArray(224 * 224 * 4);
-          // image2 = new Uint8ClampedArray(224 * 224 * 4);
-          var k = 0;
-          for(var i=0; i < output.length; i += 3) {
-            image[k++] = (.229 * output[i] + 0.485) * 225;
-            image[k++] = (.224 * output[i+1] + 0.456) * 225;
-            image[k++] = (.225 * output[i+2] + 0.406) * 225;
-            image[k++] = 255;
-          }
-          var canvas = document.createElement('canvas');
-          var ctx = canvas.getContext('2d');
+            var c = document.createElement("canvas");
+            c.width = 224
+            c.height = 224;
+            var ctx = c.getContext("2d");
+            var img = $('.file-upload-image2')[0];
+            ctx.drawImage(img, 0, 0);
+            var imgData = ctx.getImageData(0, 0, c.width, c.height);
+            secret = new Float32Array(224 * 224 * 3)
+            var k = 0;
+            for (var i = 0; i < imgData.data.length; i += 4) {
+                secret[k++] = (((imgData.data[i]) / 255.0) - 0.485) / 0.229;
+                secret[k++] = (((imgData.data[i + 1]) / 255.0) - 0.456) / 0.224;
+                secret[k++] = (((imgData.data[i + 2]) / 255.0) - 0.406) / 0.225;
+            }
+            predict_function().then(outputData => {
+                    output = outputData['hide_out']
+                    console.log(output)
+                    image = new Uint8ClampedArray(224 * 224 * 4);
+                    // image2 = new Uint8ClampedArray(224 * 224 * 4);
+                    var k = 0;
+                    for (var i = 0; i < output.length; i += 3) {
+                        image[k++] = (.229 * output[i] + 0.485) * 225;
+                        image[k++] = (.224 * output[i + 1] + 0.456) * 225;
+                        image[k++] = (.225 * output[i + 2] + 0.406) * 225;
+                        image[k++] = 255;
+                    }
+                    var canvas = document.createElement('canvas');
+                    var ctx = canvas.getContext('2d');
 
-          canvas.width = 224;
-          canvas.height = 224;
-          var i = new Image();
-          // create imageData object
-          var idata = ctx.createImageData(224, 224);
+                    canvas.width = 224;
+                    canvas.height = 224;
+                    var i = new Image();
+                    // create imageData object
+                    var idata = ctx.createImageData(224, 224);
 
-          // set our buffer as source
-          idata.data.set(image);
+                    // set our buffer as source
+                    idata.data.set(image);
 
-          // update canvas with new data
-          ctx.putImageData(idata, 0, 0);
+                    // update canvas with new data
+                    ctx.putImageData(idata, 0, 0);
 
-          var dataUri = canvas.toDataURL(); // produces a PNG file
+                    var dataUri = canvas.toDataURL(); // produces a PNG file
 
-          // i.onload = imageLoaded;       // optional callback function
-          i.src = dataUri
-          document.getElementById('completed').setAttribute('src', i.src);
-          modal.style.display = "block";
-            
+                    // i.onload = imageLoaded;       // optional callback function
+                    i.src = dataUri
+                    document.getElementById('completed').setAttribute('src', i.src);
+                    modal.style.display = "block";
 
-        })
-        .catch(err => {
-          console.log(err)
+
+                })
+                .catch(err => {
+                    console.log(err)
+                })
         })
     })
-})
-    
-
-// $('#loading').hide();
-//     $('#loaded').show();
--- js/index.js
+++ js/index.js
@@ -1,59 +1,132 @@
 function readURL1(input) {
-  if (input.files && input.files[0]) {
-    var reader = new FileReader();
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
 
-    reader.onload = function(e) {
-      $('.image-upload-wrap1').hide();
+        reader.onload = function(e) {
+            console.log(e)
+            $('.image-upload-wrap1').hide();
 
-      $('.file-upload-image1').attr('src', e.target.result);
-      $('.file-upload-content1').show();
+            $('.file-upload-image1').attr('src', e.target.result);
 
-      //$('.image-title1').html(input.files[0].name);
-    };
-    reader.readAsDataURL(input.files[0]);
-  } else {
-    removeUpload();
-  }
+            var c = document.createElement("canvas");
+            var img = e.target.result;
+            console.log(img);
+            c.width = 224;
+            c.height = 224;;
+
+            var i = new Image();
+
+            i.onload = function() {
+                console.log("printing image width")
+                console.log(i.width);
+                var ctx = c.getContext("2d");
+                var scale;
+                if (i.width > i.height)
+                    scale = 224.0 / i.width;
+                else
+                    scale = 224.0 / i.height;
+
+                console.log(scale)
+
+                ctx.scale(scale, scale);
+                ctx.drawImage(i, 0, 0);
+                console.log(ctx)
+
+                console.log(c.toDataURL());
+
+                $('.file-upload-image1').attr('src', c.toDataURL());
+
+                $('.file-upload-content1').show();
+
+                $('.image-title1').html(input.files[0].name);
+
+            };
+
+            i.src = img;
+        };
+        reader.readAsDataURL(input.files[0]);
+    } else {
+        removeUpload();
+    }
 }
 
 function removeUpload1() {
-  $('.file-upload-input1').replaceWith($('.file-upload-input1').clone());
-  $('.file-upload-content1').hide();
-  $('.image-upload-wrap1').show();
+    $('.file-upload-input1').replaceWith($('.file-upload-input1').clone());
+    $('.file-upload-content1').hide();
+    $('.image-upload-wrap1').show();
 }
-$('.image-upload-wrap1').bind('dragover', function () {
-		$('.image-upload-wrap1').addClass('image-dropping1');
-	});
-	$('.image-upload-wrap1').bind('dragleave', function () {
-		$('.image-upload-wrap1').removeClass('image-dropping1');
+$('.image-upload-wrap1').bind('dragover', function() {
+    $('.image-upload-wrap1').addClass('image-dropping1');
+});
+$('.image-upload-wrap1').bind('dragleave', function() {
+    $('.image-upload-wrap1').removeClass('image-dropping1');
 });
 
 function readURL2(input) {
-  if (input.files && input.files[0]) {
-    var reader = new FileReader();
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
 
-    reader.onload = function(e) {
-      $('.image-upload-wrap2').hide();
+        reader.onload = function(e) {
+            console.log(e)
+            $('.image-upload-wrap2').hide();
 
-      $('.file-upload-image2').attr('src', e.target.result);
-      $('.file-upload-content2').show();
+            $('.file-upload-image2').attr('src', e.target.result);
 
-      $('.image-title2').html(input.files[0].name);
-    };
-    reader.readAsDataURL(input.files[0]);
-  } else {
-    removeUpload();
-  }
+            var c = document.createElement("canvas");
+            var img = e.target.result;
+            console.log(img);
+            c.width = 224;
+            c.height = 224;;
+
+            var i = new Image();
+
+            i.onload = function() {
+
+
+
+                console.log("printing image width")
+                console.log(i.width);
+                var ctx = c.getContext("2d");
+                var scale;
+                if (i.width > i.height)
+                    scale = 224.0 / i.width;
+                else
+                    scale = 224.0 / i.height;
+
+                console.log(scale)
+
+                ctx.scale(scale, scale);
+                ctx.drawImage(i, 0, 0);
+                console.log(ctx)
+
+                console.log(c.toDataURL());
+
+                $('.file-upload-image2').attr('src', c.toDataURL());
+
+                $('.file-upload-content2').show();
+
+
+
+                $('.image-title2').html(input.files[0].name);
+
+            };
+
+            i.src = img;
+        };
+        reader.readAsDataURL(input.files[0]);
+    } else {
+        removeUpload();
+    }
 }
 
 function removeUpload2() {
-  $('.file-upload-input2').replaceWith($('.file-upload-input2').clone());
-  $('.file-upload-content2').hide();
-  $('.image-upload-wrap2').show();
+    $('.file-upload-input2').replaceWith($('.file-upload-input2').clone());
+    $('.file-upload-content2').hide();
+    $('.image-upload-wrap2').show();
 }
-$('.image-upload-wrap2').bind('dragover', function () {
-		$('.image-upload-wrap2').addClass('image-dropping2');
-	});
-	$('.image-upload-wrap2').bind('dragleave', function () {
-		$('.image-upload-wrap2').removeClass('image-dropping2');
+$('.image-upload-wrap2').bind('dragover', function() {
+    $('.image-upload-wrap2').addClass('image-dropping2');
+});
+$('.image-upload-wrap2').bind('dragleave', function() {
+    $('.image-upload-wrap2').removeClass('image-dropping2');
 });
